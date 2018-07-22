7f7ba31f11a5eaec5df234e89d7fe9d036752f04
馮元愷 <greenybox@gmail.com>
2018-07-22 19:20:01 +0800

    Initial Commit

---
+++ BuyCrystal2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BuyCrystal2
+//
+//  Created by Greenybox on 2018/7/22.
+//  Copyright © 2018年 Greenybox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
